c7d095df9c78ab6922db2b321906e931d4d7aeab
liuyujielol <2073201758GD@gmail.com>
2021-12-23 15:01:28 +0800

    add soft link shell script

---
+++ soft-link.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+#nvim
+ln -sf ./.vimrc $HOME/.config/nvim/init.vim
+ln -sf ./lua $HOME/.config/nvim/lua
+
+#zsh
+ln -sf ./.zshrc $HOME/.zshrc
+ln -sf ./zeta.zsh-theme $HOME/.oh-my-zsh/custom/themes/zeta.zsh-theme
